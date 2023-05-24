0f4685fa563be29a3bc7504e9a5189127f79c937
Chunwei Chang <chunwei.chang.cwc@gmail.com>
2023-05-24 16:45:45 -0700

    feat: test for dynamic membership

---
+++ src/test_broker_cluster_join_subscriber_info.py
@@ -0,0 +1,77 @@
+import logging
+import time
+from typing import List
+
+from broker import Broker
+from publisher import Publisher
+from utils import BROKER_HOST, BROKER_PORT
+from subscriber import Subscriber
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s [%(name)s] %(levelname)s: %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+)
+
+
+def main():
+    # broker
+    host_ips = [(BROKER_HOST, BROKER_PORT), (BROKER_HOST, BROKER_PORT + 1)]
+    brokers: List[Broker] = []
+
+    broker1 = Broker(
+        host=host_ips[0][0],
+        port=host_ips[0][1],
+        peers=[host_ips[1]],
+        election_timeout=0.5,
+    )
+    brokers.append(broker1)
+    broker2 = Broker(
+        host=host_ips[1][0],
+        port=host_ips[1][1],
+        peers=[host_ips[0]],
+        election_timeout=1,
+    )
+    brokers.append(broker2)
+
+    for broker in brokers:
+        broker.run()
+    time.sleep(2)
+
+    topic = "topic1"
+    print("==================== Subscribe to Node 1 ====================")
+    # subscriber
+    subscriber = Subscriber(*host_ips[0])
+    subscriber.run()
+    subscriber.subscribe(topic)
+    time.sleep(1)
+
+    print("==================== Node Join ====================")
+    new_node_ip = (host_ips[1][0], host_ips[1][1] + 5)
+    broker3 = Broker(
+        host=new_node_ip[0],
+        port=new_node_ip[1],
+        join_dest=host_ips[1],
+        election_timeout=1,
+    )
+    broker3.run()
+    time.sleep(2)
+
+    print("==================== Publish to New Node ====================")
+    # publisher
+    publisher = Publisher(*new_node_ip)
+    publisher.publish(topic, "Hello, world!")
+    time.sleep(1)
+
+    print("==================== Node Leave ====================")
+    broker3.stop()
+    time.sleep(2)
+
+    # stop
+    subscriber.stop()
+    for broker in brokers:
+        broker.stop()
+
+
+if __name__ == "__main__":
+    main()
